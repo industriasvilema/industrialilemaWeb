f34afc8736720dfe957d04414dae659cab78c29f
Cordova Anthony <72144025+18-anth@users.noreply.github.com>
2025-06-06 01:32:39 -0500

    config webview

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "829791936"
+    serviceWorkerVersion: "4031128543"
   }
 });
